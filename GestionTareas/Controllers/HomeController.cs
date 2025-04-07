94f1d99d24e5024604fc4f020d27088b3669fc84
HectorRec <hectorlagarda@hotmail.com.ar>
2025-04-07 11:48:10 -0700

    BD conectada al proyecto

--- GestionTareas/Controllers/HomeController.cs
+++ GestionTareas/Controllers/HomeController.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GestionTareas.DataAccessLayer;
 
 namespace GestionTareas.Controllers
 {
+
+   
     public class HomeController : Controller
     {
+
+        GestionTareasDataContext DB = new GestionTareasDataContext();
         public ActionResult Index()
         {
             return View();
@@ -26,5 +31,16 @@ namespace GestionTareas.Controllers
 
             return View();
         }
+
+        public ActionResult Tareas()
+        {
+
+            //EJEMPLO DE OBTENER UNA TAREA DE LA BASE DE DATOS.
+            var tareas = DB.Tareas.ToList().FirstOrDefault();
+            return View(tareas);
+        }
+
+
+
     }
 }
