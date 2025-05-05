1ff1c38a1d7439e50174e0bf79a69230aabcfa19
ArturoITSON <145297339+ArturoITSON@users.noreply.github.com>
2025-05-04 20:14:32 -0700

    Add files via upload

---
+++ GestionTareas/Models/Tarea.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FrontGestionTareas.Models
+{
+    public class Tarea
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "El título es obligatorio.")]
+        public string titulo { get; set; }
+
+        public string descripcion { get; set; }
+
+        [Required(ErrorMessage = "La fecha es obligatoria.")]
+        public DateTime fecha_creacion { get; set; }
+
+        public DateTime fecha_limite { get; set; }
+
+        public string estado  { get; set; }
+
+        [Required]
+        public int prioridad { get; set; }
+
+        public int id_usuario { get; set; }
+
+        public int id_lista { get; set; }
+
+
+    }
+}
