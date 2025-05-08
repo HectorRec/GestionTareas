1b7afba7f19739a2921b517de7950eaf37b04abd
ArturoITSON <sergio.garcia233316@potros.itson.edu.mx>
2025-05-07 20:47:24 -0700

    Calendario

---
+++ GestionTareas/Views/Home/Calendario.cshtml.cs
@@ -0,0 +1,26 @@
+//using FrontGestionTareas.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FrontGestionTareas.Pages
+{
+    public class CalendarioModel : PageModel
+    {
+
+        //public JsonResult OnGetTasks()
+        //{   
+            //  Aqui se obtienen las tares (por defecto esta en memoria)
+           // var eventos = TareasModel._tareasDb.Select(t => new
+          //  {
+         //       id = t.ID,
+         //       title = t.titulo,
+         //       start = t.fecha_limite.ToString("yyyy-MM-ddTHH:mm:ss"),
+         //       descripcion = t.descripcion 
+        //    }).ToList();
+
+          //  return new JsonResult(eventos);
+       // }
+    }
+
+
+}
